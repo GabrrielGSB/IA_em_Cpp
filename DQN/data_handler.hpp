968d5bcd58d3fe64ff69a968c13bc96ae9ecec15
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-10-16 13:28:06 -0300

    Update data_handler.hpp

--- DQN/data_handler.hpp
+++ DQN/data_handler.hpp
@@ -1,195 +1,46 @@
-#include "data_handler.hpp"
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
 using namespace std;
+
+// vec == vetor; rec == recurso
+// perc== porcentagem; p == para
+
+#include<fstream>
+#include "stdint.h"
+#include "data.hpp"
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_set>
+
+class data_handler
+{
+  vector<data*> *vetor_de_dados;      // Criação de ponteiros para um vetor de ponteiros
+  vector<data*> *dados_de_treinamento;
+  vector<data*> *dados_de_teste;
+  vector<data*> *dados_de_validacao;
   
-data_handler::data_handler()
-{
-	// Essa linha aloca dinamicamente um novo vetor que será usado para armazenar ponteiros...
-	// para objetos da classe "data". O ponteiro para esse vetor é então atribuído à variável "lista_de_dados".
-	lista_de_dados 	     = new vector<data*>; // inicializa as variáveis definidas no arquivo "hpp".
-	dados_de_teste       = new vector<data*>; // Aloca espaço na memória que pode ser expandido e contraído dinamicamente.
-	dados_de_treinamento = new vector<data*>;
-    dados_de_validacao   = new vector<data*>; 
-}
+  int num_classes;                  // Numero de classes presentes nos dados
+  int tamanho_do_vec_de_rec;
+  map<uint8_t, int> mapa_da_classe; // cria um mapeamento de valores "uint8_t" para "int", como uma classe python
 
-data_handler::~data_handler(){};
+  const double PERC_DE_TREINAMENTO = 0.75; // poncentagem de treinamento, teste e validação na rede neural
+  const double PERC_DO_TESTE       = 0.20;
+  const double PERC_DE_VALIDACAO   = 0.05;
 
-void data_handler::ler_vec_de_rec(string path)
-{
-	uint32_t header[4]; // guarda o "magic number", "numero de imagens", "numero de linhas" e "numero de colunas" do arquivo a ser lido.
-	unsigned char bytes[4];
-	// path.c_str(): Se path for uma variável do tipo std::string (como é comum em C++), o método c_str() converte a "std::string"...
-	// para um array de caracteres (const char*) que pode ser passado para funções da biblioteca C, como fopen.
-	FILE *f = fopen(path.c_str(), "r"); // "r" arquivo aberto no modo de leitura, não pode ser modificado.
-	
-	if(f) // checa se o arquivo conseguiu ser aberto e lido.
-	{
-		for(int i = 0; i < 4; i++)
-		{
-			if(fread(bytes, sizeof(bytes), 1, f))
-			{
-				header[i] = converta_para_pequeno_indian(bytes);			
-			}
-		}
+  public:
+  data_handler();
+  ~data_handler();
 
-		printf("Termino da obtenção do cabeçalho do arquivo.\n");
-		int tamanho_da_imagem = header[2]*header[3]; // produto do número de linhas pelo número de colunas em pixels.
+  void ler_vec_de_rec(string path); 
+  void ler_labels_de_recursos(string path);
+  void dividir_dados(); // Função para dividir os dados de acordo com as const definidas acima.
+  void contar_classes();
 
-		for(int i = 0; i < header[1]; i++) // iteração sobre o num de imagens
-		{
-			data *d = new data(); // alocando espaço na memória para um objeto da classe "data" e referindo um ponteiro para ele.
-			uint8_t elemento[1];
-
-			for(int j = 0; j < tamanho_da_imagem; j++) // itera sobre o tamanho da imagem para ler pixel por pixel
-			{
-				if(fread(elemento, sizeof(elemento), 1, f)) {d -> incluir_no_vec_de_rec(elemento[0]);} // lê um pixel no arquivo
-				else 
-				{
-					printf("Erro na leitura do arquivo!!!\n");
-					exit(1);
-				}		
-			}
-		  	lista_de_dados -> push_back(d);
-		}
-		prinf("A leitura e salvamento dos %lu vetores de recurso foi um sucesso.\n", lista_de_dados -> size());
-	} 
-	else 
-	{
-		prinf("Arquivo não encontrado!!!\n");
-		exit(1);
-	}
-}
-void data_handler::ler_labels_de_recursos(string path)
-{
-	uint8_t header[2];
-	unsigned char bytes[4];
-	FILE *f = fopen(path.c_str(), "r");
-	
-	if(f)
-	{
-		for(int i = 0; i < 2; i++)
-		{
-			if(fread(bytes, sizeof(bytes), 1, f))
-			{
-				header[i] = converta_para_pequeno_indian(bytes);			
-			}
-		}
-		printf("Termino da obtenção da label do cabeçalho do arquivo.\n");
-		for(int i = 0; i < header[1] ; i++)
-		{	
-			uint8_t elemento[1];
-			if(fread(elemento, sizeof(elemento, 1, f)))
-			{
-				vetor_de_dados->at(i)->definir_label(elemento[0]);
-			}	else 
-			{
-				printf("Erro na leitura do arquivo!!!\n");
-				exit(1);
-			}		
-		}
-		prinf("A leitura e salvamento da label foi um sucesso.\n");
-	} else 
-	{
-		prinf("Arquivo não encontrado!!!\n");
-		exit(1);
-	}
-}
-
-void data_handler::dividir_dados()
-{
-	unordered_set<int> indices_usados;
-	int tamanho_do_treinamento = vetor_de_dados->size() * PORCENTAGEM_DE_TREINAMENTO
-	int tamanho_do_teste       = vetor_de_dados->size() * PORCENTAGEM_DO_TESTE
-	int tamanho_da_validacao   = vetor_de_dados->size() * PORCENTAGEM_DE_VALIDACAO
-
-	// Dados de Treinamento
-
-	int count = 0;
-	while(count < tamanho_do_treinamento)
-	{
-		int rand_indice = rand() % vetor_de_dados	->size();
-		if(indices_usados.find(rand_indice) == indices_usados.end())
-		{
-			dados_de_treinamento->push_back(vetor_de_dados->at(rand_indice));
-			indices_usados.insert(rand_indice);
-			count++;	
-		}
-	}
-
-	// Dados de Teste
-
-	int count = 0;
-	while(count < tamanho_do_teste)
-	{
-		int rand_indice = rand() % vetor_de_dados	->size();
-		if(indices_usados.find(rand_indice) == indices_usados.end())
-		{
-			dados_de_teste->push_back(vetor_de_dados->at(rand_indice));
-			indices_usados.insert(rand_indice);
-			count++;
-		}
-	}
-
-	// Dados de Validação
-
-	int count = 0;
-	while(count < tamanho_da_validacao)
-	{
-		int rand_indice = rand() % vetor_de_dados	->size();
-		if(indices_usados.find(rand_indice) == indices_usados.end())
-		{
-			dados_de_validacao->push_back(vetor_de_dados->at(rand_indice));
-			indices_usados.insert(rand_indice);
-			count++;
-		}
-	}
-
-	printf("Tamanho dos dados de treinamento %lu.\n", dados_de_treinamento);
-	printf("Tamanho dos dados de teste %lu.\n",       dados_de_teste);
-	printf("Tamanho dos dados de validação %lu.\n",   dados_de_validação);
-}
-void data_handler::contar_classes()
-{
-	int count = 0;
-	for (unsigned i = 0; i < lista_de_dados->size(); i++)
-	{
-		if(class_map.find(lista_de_dados->(i)->obter_label()) == class_map.end())
-		{
-			class_map[lista_de_dados->at(i)->obter_label()] = count;
-			lista_de_dados->at(i)->definir_label_numerada(count);
-			count++;
-		}
-	}
-	num_classes = count;
-	printf("Foram obtidicas %d classes únicas.\n", num_classes);
-}
-
-uint32_t data_handler::converta_para_pequeno_endian(const unsigned char* bytes)
-{
-	return(uint32_t) ((byte[0] << 24) |
-										(byte[1] << 16) |
-										(byte[2] << 8 ) |
-										(byte[3]));
-}
+	uint32_t conv_p_little_endian(const unsigned char* bytes); // converte um dado para o formato little endian, onde...
+                                                             // o byte menos significativo vem primeiro na memória.
   
-vector<data *> * data_handler::obter_dados_de_treinamento()
-{
-	return dados_de_treinamento;
-}
-
-vector<data *> * data_handler::obter_dados_de_teste()
-{
-	return dados_de_teste;
-}
-vector<data *> * data_handler::obter_dados_de_validacao()
-{
-	return dados_de_validacao;
-}
-
-int main()
-{
-	data_handler *dh = new data_handler();
-	dh->ler_vetor_de_recursos("./NOME_DO_ARQUIVO");
-	dh->ler_labels_de_recursos("./NOME_DO_ARQUIVO");
-	dh->dividir_dados();
-	dh->contar_classes();
-}
+	vector<data*> * obter_dados_de_treinamento();	 // retorna os dados de treinamento, teste, e validação no formato de...
+	vector<data*> * obter_dados_de_teste();        // ponteiro de vetor de ponteiros
+	vector<data*> * obter_dados_de_validacao();
+};
