63025db91d7ebbac6bf8c31e3b55b0d5fed98c01
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-11-21 07:41:20 -0300

    Update Neuronio.cpp

--- MLP/src/Neuronio.cpp
+++ MLP/src/Neuronio.cpp
@@ -10,25 +10,37 @@ float gerarNumAleatorio(float min, float max)
     return min + aleatorio * (max - min);
 }
 		
-void  Neuronio::inicializarPesos()
+void  Neuronio::inicializarPesos(string mode)
 {
-	default_random_engine gerador;
-    normal_distribution<float> distribuicao(0.0, 1.0);
+	if (mode == "random")
+	{
+		default_random_engine gerador;
+    	normal_distribution<float> distribuicao(0.0, 1.0);
     
-    for(int i = 0; i < tamEntrada + 1; i++ )
-    {
-    	pesos.push_back(gerarNumAleatorio(0, 1.0));
-    }
+    	for(int i = 0; i < tamEntrada + 1; i++ )
+    	{
+    		pesos.push_back(gerarNumAleatorio(0.0, 1.0));
+    	}
+		// for (float j : pesos) printf("%.2f", j);
+	}
+	else if (mode == "zero")
+	{
+		for(int i = 0; i < tamEntrada + 1; i++ )
+    	{
+    		pesos.push_back(0);
+    	}
+	}
+	
 }	
 
-void  Neuronio::definirTaxaAprendizado(float TaxaAprendizado)
+void  Neuronio::definirTaxaAprendizado(float taxaAprend)
 {
-	taxaAprendizado = taxaAprendizado;
+	taxaAprendizado = taxaAprend;
 }
 
 float Neuronio::degrau(float u, string derivada="false") 
 { 
-	return u > 0 ? 1 : 0; 
+	return u > 0 ? 1.0 : 0.0; 
 }
 
 void  Neuronio::aplicarFuncAtivacao(float y, string funcAtiv)
@@ -38,32 +50,33 @@ void  Neuronio::aplicarFuncAtivacao(float y, string funcAtiv)
 
 void  Neuronio::aplicarEntrada(vector<float> entrada, string funcAtiv)
 {
-	entrada.insert(entrada.begin(), -1);
-
+	U = {};
+	entrada.insert(entrada.begin(), -1.0);
+	// printf("\n");
 	for (int i = 0; i < entrada.size(); i++)
 	{ 
-		U.push_back(entrada[i+1] * pesos[i+1]);
+		U.push_back(entrada[i] * pesos[i]);
 	}
 
-	saida = accumulate(U.begin(), U.end(), 0.0) - pesos[0];
-
+	saida = accumulate(U.begin(), U.end(), 0.0);
+	// printf("%f \n", saida);
 	aplicarFuncAtivacao(saida, funcAtiv);
 }
 
 
 // int main()
 // {
-// 	Neuronio n(2, 1, "h");
-// 	n.inicializarPesos();
-// 	n.aplicarEntrada({2.0,2.0}, "degrau");
-// 	for (float c : n.entrada) printf("%.3f\n", c);
+// 	Neuronio n(2, "degrau");
+// 	n.inicializarPesos("random");
+// 	n.aplicarEntrada({1,1}, "degrau");
+// 	for (float c : {1,1}) printf("%.10f\n", c);
 // 	printf("\n");
-// 	for (float a : n.pesos) printf("%.3f\n", a);
+// 	for (float a : n.pesos) printf("%.10f\n", a);
 // 	printf("\n");
-// 	for (float b : n.U) printf("%.3f\n", b);
+// 	for (float b : n.U) printf("%.10f\n", b);
 // 	printf("\n");
 // 	printf("%.4f", n.saida);
-// 	 return 0;			
+// 	return 0;			
 // }					
 
 
