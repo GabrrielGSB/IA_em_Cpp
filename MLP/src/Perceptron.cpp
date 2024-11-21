dd7366da7599f66a6fe3c0b7d5e62016578e00f3
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-11-21 07:40:41 -0300

    Update Perceptron.cpp

--- MLP/src/Perceptron.cpp
+++ MLP/src/Perceptron.cpp
@@ -1,32 +1,46 @@
 #include "../include/Perceptron.h"
 
-Perceptron::Perceptron(int tamEntrada, string funcAtiv="degrau") : n(tamEntrada, funcAtiv){}
+Perceptron::Perceptron(int tamEntrada, string funcAtiv="degrau") : 
+                       n(tamEntrada, funcAtiv), tamEntrada(tamEntrada), episodios(0), existeErro(false) {}
 
-void Perceptron::atualizarPesos(float taxaAprendizado, vector<float> entrada, vector<float> saidaDesejada)
+void Perceptron::atualizarPesos(float taxaAprendizado, vector<float> entrada, float saidaDesejada)
 {
+    printf("\n");
+    n.definirTaxaAprendizado(taxaAprendizado);
+    entrada.insert(entrada.begin(), -1.0);
+
+
     for (int i = 0; i < n.pesos.size(); i++)
-    {
-        n.pesos[i] = n.pesos[i] + n.taxaAprendizado * (saidaDesejada[i] - n.saida) * entrada[i];
+    {   
+        printf("velho peso %d: %.15f // ", i, n.pesos[i]);
+        n.pesos[i] = n.pesos[i] + 0.1 * (saidaDesejada - n.saida) * entrada[i];
+        printf("novo peso %d: %.15f // ", i, n.pesos[i]);
+        printf("\n");
+        printf("Saida desejada: %.3f // ", saidaDesejada);
+        printf("Saida: %.3f // ", n.saida);
+        printf("Entrada: %.3f", entrada[i]);
+        printf("\n");
     }
     existeErro = true;
 }
 
 void Perceptron::treinar(float taxaAprendizado, vector<vector<float>> entradas, vector<float> saidaDesejada)
 {
-    episodios = 0;
-    n.inicializarPesos();
+    n.inicializarPesos("random");
 
     n.definirTaxaAprendizado(taxaAprendizado);
 
     do
     {
         existeErro = false;
+
         for (int i = 0; i < entradas.size(); i++)
         {
             n.aplicarEntrada(entradas[i], "degrau");
+
             if (n.saida != saidaDesejada[i]) 
             {
-                atualizarPesos(taxaAprendizado, entradas[i], saidaDesejada);   
+                atualizarPesos(taxaAprendizado, entradas[i], saidaDesejada[i]);   
             }
         }
         episodios++;
@@ -39,6 +53,7 @@ void Perceptron::mostrarResultados(vector<vector<float>> entradas)
     for (int i = 0; i < entradas.size(); i++)
     {
         n.aplicarEntrada(entradas[i], "degrau");
+        printf("\n");
         printf("%.1f ", n.saida);
     }
 }
@@ -46,11 +61,11 @@ void Perceptron::mostrarResultados(vector<vector<float>> entradas)
 int main()
 {
     vector<vector<float>> entradas = {{0.0, 0.0}, {0.0, 1.0}, {1.0, 0.0}, {1.0, 1.0}};
-    vector<float> saidasDesejadas  = {0.0, 1.0, 1.0, 1.0};
+    vector<float> saidasDesejadas  = {0.0, 0.0, 0.0, 1.0};
 
     Perceptron p(2);
 
-    p.treinar(1, entradas, saidasDesejadas);
+    p.treinar(0.1, entradas, saidasDesejadas);
 
     p.mostrarResultados(entradas);
 
