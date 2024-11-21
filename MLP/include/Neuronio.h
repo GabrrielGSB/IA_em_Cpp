3ae718b16ed11b9e730de987affe62765e95b9ba
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-11-20 21:03:01 -0300

    Add files via upload

---
+++ MLP/include/Neuronio.h
@@ -0,0 +1,32 @@
+#ifndef __NEURONIO_HPP
+#define __NEURONIO_HPP
+
+using namespace std;
+#include <vector>
+#include <string>
+#include <numeric>
+#include <algorithm>
+
+class Neuronio
+{
+	public:
+		vector<float> pesos, U;
+		float saida, gradienteLocal, taxaAprendizado;
+		int tamEntrada;
+		string funcAtiv;
+
+		Neuronio(int tamEntrada, string funcAtiv);
+		
+		void inicializarPesos();	
+		void definirTaxaAprendizado(float TaxaAprendizado);
+		void aplicarFuncAtivacao(float saida, string funcAtiv);
+		void aplicarEntrada(vector<float> input, string funcAtiv);
+		//void mudarAtivacao();
+		// vector<float> obterPesos();
+
+		// FUNÇÕES DE ATIVAÇÃO-------------------------
+		float degrau(float u, string derivada);
+		// void sigmoid();
+};
+
+#endif
